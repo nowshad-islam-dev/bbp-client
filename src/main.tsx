d162a09824debd803e570847b1e0c5a592a0c39b
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-11-24 14:56:35 +0600

    add redux store with initial setup for rtk query and mdb react ui kit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from '@/App';
+import store from '@/store/store';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </StrictMode>,
 );
