87158d49753a0972483d8c311a38663d981ca033
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-12-08 13:55:07 +0600

    change boundary value of  this hook

--- src/hooks/useMobile.tsx
+++ src/hooks/useMobile.tsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from 'react';
 
 // Helper: detect screen width
 export const useIsMobile = () => {
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
     useEffect(() => {
-        const handleResize = () => setIsMobile(window.innerWidth < 992);
+        const handleResize = () => setIsMobile(window.innerWidth < 768);
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, []);
